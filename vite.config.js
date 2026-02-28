0feb80fbb40351592c3f73ad966692714e5b45d5
Hiral <hiralchuahan7574964996@gmail.com>
2026-02-28 11:36:51 +0530

    front

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,5 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  base: "/SEM-5_REACT_PROJECT/",
-  
+  base: "/Internship_project/",
 });
